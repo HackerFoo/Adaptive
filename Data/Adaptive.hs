384c20e18417cfa322212f88f7c771a01f4267f0
Dustin DeWeese <dustin.deweese@gmail.com>
2012-06-12 23:04:50 -0400

    add license header

--- Data/Adaptive.hs
+++ Data/Adaptive.hs
@@ -1,3 +1,20 @@
+{-  
+    This file is part of Adaptive.
+
+    Adaptive is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Adaptive is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with Adaptive.  If not, see <http://www.gnu.org/licenses/>.
+-}
+
 {-# LANGUAGE FlexibleInstances, BangPatterns, UnboxedTuples #-}
 {-# OPTIONS_GHC -fno-excess-precision -fno-spec-constr #-}
 -- {-# OPTIONS_GHC -fglasgow-exts -fno-excess-precision -fno-spec-constr #-}
